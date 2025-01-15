18d19052d9290a10afc647dc64d12d1f7f179a49
FadySabry712 <fadysabry95@yahoo.com>
2025-01-15 15:42:40 -0800

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Using Puppet, create a file in /tmp
+
+file { '/tmp/school':
+  mode    =>'0744',
+  owner   => 'www-data',
+  gorup   => 'www-data',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+ this is a puppet learning task by Holberton

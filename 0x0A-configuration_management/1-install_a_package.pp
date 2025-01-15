51ebdb39fb4b5513b6b5d5d35ab08c725f4d2290
FadySabry712 <fadysabry95@yahoo.com>
2025-01-15 15:53:33 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
 #Using Puppet, install flask from pip3
 
+# ensure python is present 
+package { 'python3.8':
+  ensure => present,
+}
+
+# ensure pip is present
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',

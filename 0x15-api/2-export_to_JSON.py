263af1f9ad91c7c11af3de59715d6f19a5b53b9b
FadySabry712 <fadysabry95@yahoo.com>
2025-02-12 12:14:28 -0800

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Exports data to JSON file format """
+
+import sys
+import requests
+import json
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    employee_response = requests.get(employee_url)
+    username = employee_response.json().get('username')
+
+    t_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    todos_response = requests.get(t_url)
+    tasks = todos_response.json()
+    dictionary = {employee_id: []}
+    for task in tasks:
+        dictionary[employee_id].append({
+            "tasks": task.get("title"),
+            "completed": task.get("completed"),
+            "username": username
+        })
+    with open(f'{employee_id}.json', 'w') as f:
+        json.dump(dictionary, f)

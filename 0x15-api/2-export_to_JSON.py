a959cfb202384ad864ee0a0b9fbf2ab93ac8b284
FadySabry712 <fadysabry95@yahoo.com>
2025-02-12 12:16:39 -0800

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Exports data to JSON file format """
 
-import sys
-import requests
 import json
+import requests
+import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
